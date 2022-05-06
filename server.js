05e724c0a0c56e2fb09799bfb4ed7f646c050cad
LuisJr <luisjr_00@hotmail.com>
2022-05-05 21:39:23 -0300

    AtvSemana12

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var app = require('./config/express')();
+const url = 'mongodb://dswa5:dswa5@clusterdswa5-shard-00-00.r9mcd.mongodb.net:27017,clusterdswa5-shard-00-01.r9mcd.mongodb.net:27017,clusterdswa5-shard-00-02.r9mcd.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-q27y5k-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express Server escutando na porta ' + app.get('port'));
+});
